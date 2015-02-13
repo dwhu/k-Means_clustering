8c3682dd709129ea1fd3ad497dffd800500a234a
David Hughes <hughdw11@wfu.edu>
2015-02-13 11:43:38 -0500

    Working parallelized kMeans for small cluster

--- kMeans.c
+++ kMeans.c
@@ -13,16 +13,20 @@ for K Means Lab
 #include <limits.h>
 
 
-#define NUM_CLUSTERS 40
-#define NUM_THREADS 50
+#define NUM_CLUSTERS 50
+#define NUM_THREADS 4
+#define ERROR_THRESHOLD 10
 
 /* kdata.idat */
 #define MAX_NUMS 201
 #define FNAME "kdata.idat"
 
+typedef int bool;
+enum { false, true };
+
 struct means{
-	int x;
-	int y;
+	long x;
+	long y;
 };
 
 typedef struct means means;
@@ -34,7 +38,7 @@ typedef struct means means;
 		/* Shared variables */
 		int map [MAX_NUMS][MAX_NUMS];
 		means global_means[NUM_CLUSTERS];
-        int global_totals[NUM_CLUSTERS];
+        long global_totals[NUM_CLUSTERS];
         int finished = 0;
 
 		/* Private variables */
@@ -54,11 +58,12 @@ typedef struct means means;
 		FILE * fp;
         clock_t start, diff;
         
-        start = clock();
         
 	/*  Master thread sets up */
         #pragma omp master
         {
+            start = clock();
+            
             fp = fopen(FNAME,"r");
             if(fp == NULL)
             {
@@ -90,30 +95,36 @@ typedef struct means means;
                     y++;
                     x = 0;
                     /* Debug */
-                   /*  printf("\n",x,y,tmp); */
+                   /* printf("\n",x,y,tmp); */
 
                 }
             } while (r != EOF);
             
+          /*  printf("\n\n\n\n\n"); */
+            
             /* Close File */
             fclose(fp);
+        
+            
+            global_means[0].x = 0;
+            global_means[0].y = 0;
+            global_totals[0] = 0;
             
             /* Debug */
-            printf("\n\n\nMean Indexs:\n\n[");
+            printf("Mean Indexs:\n\n[(0,0)");
             
-            for(mean_index = 0; mean_index < NUM_CLUSTERS; mean_index++) {
+            for(mean_index = 1; mean_index < NUM_CLUSTERS; mean_index++) {
                 /* Generate random points to start out as the main points of our clusters */
                 
-                x = rand() % MAX_NUMS;
-                y = rand() % MAX_NUMS;
+                x = abs(rand()) % MAX_NUMS;
+                y = abs(rand()) % MAX_NUMS;
                 
                 /* Debug */
-                printf("(%d,%d),",x,y);
+                printf(",(%d,%d)",x,y);
                 
                 global_means[mean_index].x = x;
                 global_means[mean_index].y = y;
                 global_totals[mean_index] = 0;
-                mean_index++;
             }
             /* Debug */
             printf("]\n\n");
@@ -151,11 +162,11 @@ typedef struct means means;
             
             /* Debug */
             /*  Check in to make sure space allocated correctly */
-           printf("(%d): chunk_size: %d, startY: %d endY: %d\n",id,chunk_size,start_Y,end_Y);
+          /* printf("(%d): chunk_size: %d, startY: %d endY: %d\n",id,chunk_size,start_Y,end_Y); */
             
             /* k-Means Portion */
             n = 0;
-       /*     do { */
+            do {
                 /*Sync Everyone Up to read in new means*/
                 #pragma omp barrier
                 
@@ -167,8 +178,8 @@ typedef struct means means;
                 int mean_index;
                 for (mean_index = 0; mean_index < NUM_CLUSTERS; mean_index++) {
                     /* Set up a local copy for the thread */
-                    local_means[mean_index].x = global_means[mean_index].x;
-                    local_means[mean_index].y = global_means[mean_index].y;
+                    local_means[mean_index].x = 0;
+                    local_means[mean_index].y = 0;
                     local_totals[mean_index] = 0;
                     
                     /* Also make a copy of the last round so we can verify
@@ -188,27 +199,25 @@ typedef struct means means;
                     /* Columns */
                     for (x = 0; x < MAX_NUMS; x++) {
                         
-                        /* Holds Shortest distance to mean recorded 
-                         and the index of that mean */
-                        int shortest_distance = INT_MAX;
-                        int shortest_mean_index;
-                        
                         /* If the current value is not a zero then recluster it */
                         if(map[x][y] != 0){
+                            
+                            /* Holds Shortest distance to mean recorded
+                             and the index of that mean */
+                            double shortest_distance = DBL_MAX;
+                            int shortest_mean_index = 0;
+                            
                             /* Calculate shortest distance to a mean */
                             for (mean_index=1; mean_index < NUM_CLUSTERS; mean_index++) {
                                 
-                                
-                                int x_Distance= last_means[mean_index].x - x;
-                                x_Distance *= x_Distance;
-                                int y_Distance = last_means[mean_index].y -y;
-                                y_Distance *= y_Distance;
+                                double x_Distance= abs(last_means[mean_index].x - x);
+                                double y_Distance = abs(last_means[mean_index].y -y);
                                 
                                 /* eucliean_distance = (mean_X - this_Y)^2 + (mean_Y -this_Y)^2 */
-                                int euclidian_distance = x_Distance + y_Distance;
+                                double euclidian_distance = (pow(x_Distance,2) + pow(y_Distance,2));
                                 
                                 /* If the distance is shorter, mark it */
-                                if (euclidian_distance < shortest_distance) {
+                                if (euclidian_distance <= shortest_distance) {
                                     shortest_distance = euclidian_distance;
                                     shortest_mean_index = mean_index;
                                 }
@@ -219,8 +228,10 @@ typedef struct means means;
                             
                             /* Update the means before processing */
                             local_totals[shortest_mean_index] ++;
-                            local_means[shortest_mean_index].x = x;
-                            local_means[shortest_mean_index].y = y;
+                            local_means[shortest_mean_index].x += x;
+                            local_means[shortest_mean_index].y += y;
+                            
+                            /*printf("(%d) Mean: %d = %d\n",id,shortest_mean_index,local_totals[shortest_mean_index]); */
                             
                             /* Mark node as belonging to that cluster
                              shortest_mean_index = cluster_number 
@@ -230,10 +241,24 @@ typedef struct means means;
                              to the same area.
                              */
                             map[x][y] = shortest_mean_index;
+                           /* printf("(%d,%d) = %d\n",x,y,shortest_mean_index); */
                         }
                     }
                 }
+            
+         /*   for(mean_index = 0; mean_index < NUM_CLUSTERS; mean_index++){
                 
+                #pragma omp barrier
+                
+                #pragma omp critical
+                {
+                printf("(%d) mean:(%d,%d) x:%d y:%d total:%d\n",id,last_means[mean_index].x,last_means[mean_index].y,local_means[mean_index].x,local_means[mean_index].y,local_totals[mean_index]);
+                }
+            }*/
+            
+            
+            /* Synconyze everyone */
+            #pragma omp barrier
                 /* Debug */
                /* printf("\n\n{%d} Critical Section. Run: %d\n\n",id,n); */
                 
@@ -241,7 +266,7 @@ typedef struct means means;
                 /* Each thread updates its means and cluster */
                 #pragma omp critical
                 {
-                    for(mean_index = 0; mean_index< NUM_CLUSTERS; mean_index++){
+                    for(mean_index = 1; mean_index< NUM_CLUSTERS; mean_index++){
                         global_means[mean_index].x += local_means[mean_index].x;
                         global_means[mean_index].y += local_means[mean_index].y;
                         global_totals[mean_index] += local_totals[mean_index];
@@ -259,33 +284,50 @@ typedef struct means means;
                     
                     int mean_stayed_same [NUM_CLUSTERS];
                     /* Update Global Means */
-                    for(mean_index = 0; mean_index< NUM_CLUSTERS; mean_index++){
+                    for(mean_index = 1; mean_index< NUM_CLUSTERS; mean_index++){
                         
                         if(global_totals[mean_index] != 0){
-                            global_means[mean_index].x = global_means[mean_index].x/global_totals[mean_index];
-                            global_means[mean_index].y += global_means[mean_index].y/global_totals[mean_index];
+                       
+                            /* printf("x: %d y: %d total: %d\n",global_means[mean_index].x,global_means[mean_index].y,global_totals[mean_index]); */
+                            
+                            global_means[mean_index].x = global_means[mean_index].x/(global_totals[mean_index] +1);
+                            global_means[mean_index].y = global_means[mean_index].y/(global_totals[mean_index] + 1);
                             global_totals[mean_index] = 0;
                         }
                         
                         /* Debug */
                       /*  printf("(%d,%d),",global_means[mean_index].x,global_means[mean_index].y); */
                         
-                        if(global_means[mean_index].x == last_means[mean_index].x &&
-                           global_means[mean_index].y == last_means[mean_index].y){
-                            mean_stayed_same[mean_index] = 1;
+                        /* We use this Error threshold because of interger roundoff when divising to find our
+                        interger divsion.  This should compensate for that.  Also we really dont care about 
+                         small changes to the clusters*/
+                        if(abs(global_means[mean_index].x - last_means[mean_index].x) < ERROR_THRESHOLD &&
+                           abs(global_means[mean_index].y - last_means[mean_index].y) < ERROR_THRESHOLD){
+                            mean_stayed_same[mean_index] = true;
                         }else{
-                            mean_stayed_same[mean_index] = 0;
+                            mean_stayed_same[mean_index] = false;
                         }
                     }
                     /* Debug */
                   /*  printf("]"); */
                     /* Compare New Means to Old Means */
                     finished = and_bool_array(mean_stayed_same,NUM_CLUSTERS);
+                    
+                    
+                    /* Debug */
+                    printf("Run: %d NUM_CLUSTERS: %d\nMean Clusters: [",n,NUM_CLUSTERS);
+                    
+                    for(mean_index = 0; mean_index < NUM_CLUSTERS; mean_index++){
+                        printf("(%d,%d),",global_means[mean_index].x,global_means[mean_index].y);
+                    }
+                    
+                    printf("]\n\n");
                 }
                 n++;
                 
-                
-                
+               /* Make everyone wait for master to finish */
+                #pragma omp barrier
+               
                 /* Debug */
                 /* Print the Map */
              /*   printf("Printing the Map:\n\n");
@@ -296,21 +338,24 @@ typedef struct means means;
                     printf("\n");
                 }
             */
-     /*       } while (!finished); */
+               
+          } while (!finished);
             
         } /* End Parallel Section */
 
         
-        diff = clock() - start;
-        int msec = diff * 1000 / CLOCKS_PER_SEC;
-        printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
+       
         
         #pragma omp master
         {
+            diff = clock() - start;
+            int msec = diff * 1000 / CLOCKS_PER_SEC;
+            printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
+            
             printf("Printing the Map:\n\n");
             for (y=0; y<MAX_NUMS; y++) {
                 for (x=0; x < MAX_NUMS; x++) {
-                    printf("%d",map[x][y]);
+                    printf("(%d)",map[x][y]);
                 }
                 printf("\n");
             }
@@ -322,10 +367,10 @@ typedef struct means means;
 /* Performs a logical and of a bool array */
 int and_bool_array(int array [], int size){
     int x;
-    for (x=0; x<size; x++) {
+    for (x=1; x<size; x++) {
         if(!array[x]){
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
